f7db1109d4cc01db24246fd11411168fd80f7147
mariaclara0609 <up202406088@up.pt>
2025-04-24 13:47:49 +0100

    Criação da classe Automovel e variáveis pertencentes à mesma.

---
+++ src/Automovel.java
@@ -0,0 +1,11 @@
+public class Automovel {
+    private String matricula;
+    private String marca;
+    private int cilindrada;
+
+    private static final String MATRICULA_POR_OMISSAO = "sem matrícula";
+    private static final String MARCA_POR_OMISSAO = "sem marca";
+    private static final int CILINDRADA_POR_OMISSAO = 0;
+
+    private static int totalAutomoveis = 0;
+}
